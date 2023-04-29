f3c914ba56c2601f3433b50eaf1083b31ab20af7
Cédric Marty <cmarty23@students.foi.hr>
2023-04-29 19:46:44 +0200

    #4 DisplayData

--- text_file_to_ui/UserDisplayer.cs
+++ text_file_to_ui/UserDisplayer.cs
@@ -17,8 +17,34 @@ namespace text_file_to_ui
         }
         private void displayField(Field field, UserInfoForm info)
         {
+            switch (field.name) 
+            {
+                case "Ime":
+                case "Name":
+                    info.name.value = field.value;
+                    break;
+                case "Surname":
+                    info.name.value = info.name.value + " " + field.value;
+                    break;
+                case "Year of birth":
+                    info.yearOfBirth.value = field.value;
+                    break;
+                case "City of origin":
+                    info.city.value = field.value;
+                    break;
+                case "Faculty":
+                    info.faculty.value = field.value;
+                    break;
+                case "Role":
+                    info.role.value = field.value;
+                    break;
+                case "Favorite course":
+                case "Department":
+                    info.specificAttribute.value = field.value;
+                    info.specificAttribute.name = field.name;
+                    break;
 
-
+            }
         }
 
     }
