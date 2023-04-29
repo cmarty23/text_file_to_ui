c65b0dc7ac5b95ae116eb32f9e52fddba6dceab6
Cédric Marty <cmarty23@students.foi.hr>
2023-04-29 17:52:19 +0200

    #5 write UserFileReader class

--- text_file_to_ui/Form1.cs
+++ text_file_to_ui/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,21 @@ namespace text_file_to_ui
 {
     public partial class Form1 : Form
     {
+        UserInfoForm userInfoForm;
+        UserFileReader reader;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                reader.filePath = openFileDialog1.FileName;
+                reader.readUserFile();
+            }
         }
     }
 }
--- text_file_to_ui/UserFileReader.cs
+++ text_file_to_ui/UserFileReader.cs
@@ -3,21 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace text_file_to_ui
 {
     internal class UserFileReader
     {
         public string filePath;
-        public string fileContent;
+        public string[] fileContent;
 
         public void readUserFile()
         {
-            return;
+            fileContent = File.ReadAllLines(filePath);
+            incrementFileOpenCounter();
         }
         private void incrementFileOpenCounter()
         {
-            return;
+            string lastLine = fileContent[fileContent.Length - 1];
+            int idx = lastLine.LastIndexOf('=');
+
+            if (idx == -1)
+            {
+                return; //TODO catch error
+            }
+            int newCount = int.Parse(lastLine.Substring(idx + 1))+1;
+
+            fileContent[fileContent.Length - 1] = lastLine.Substring(0, idx) + newCount.ToString();
+
+            File.WriteAllLines(filePath, fileContent);
         }
     }
 }
