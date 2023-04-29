aa7b63c406648ad34694043248c8b11789fe9484
Cédric Marty <cmarty23@students.foi.hr>
2023-04-29 20:00:02 +0200

    Update window name

--- text_file_to_ui/Form1.Designer.cs
+++ text_file_to_ui/Form1.Designer.cs
@@ -173,7 +173,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "User data display";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
